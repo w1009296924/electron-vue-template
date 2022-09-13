07e3fc4f4406307a01d311f1f416107471edec0b
xxuss855 <760970028@qq.com>
2022-09-13 20:57:31 +0800

    待办页修改

--- src/renderer/views/pending/components/pendingList.vue
+++ src/renderer/views/pending/components/pendingList.vue
@@ -1,12 +1,29 @@
 <template>
 <div class="container">
-  <div class="date">{{detail.date}}</div>
-  <div class="missionName">
-    <i v-if="detail.children" class="el-icon-arrow-right" :class="[clickFlag?'rightArrow':'downArrow']" @click="expandList" />
-    {{detail.missionName}}
+  <div class="father">
+    <div class="missionName">
+      <i v-if="detail.children" class="el-icon-arrow-right" :class="[clickFlag?'rightArrow':'downArrow']" @click="expandList" />
+      {{detail.missionName}}
+    </div>
+    <div class="date">{{detail.date}}</div>
+    <div class="pendingName">{{detail.pendingName}}</div>
+    <el-checkbox v-model="detail.status" class="checkbox" @change="change" />
   </div>
-  <div class="pendingName">{{detail.pendingName}}</div>
-  <el-checkbox v-model="checked" class="checkbox" @change="change" />
+  <div v-if="detail.children&&clickFlag">
+    <div v-for="(item,index) of detail.children" v-if="!item.status" :key="index" class="children">
+      <div class="missionName"></div>
+      <div class="date">{{item.date}}</div>
+      <div class="pendingName">{{item.pendingName}}</div>
+      <el-checkbox v-model="item.status" class="checkbox" @change="changeChildren($event,index)" />
+    </div>
+    <div v-for="(item,index) of detail.children" v-if="item.status" :key="index" class="children">
+      <div class="missionName"></div>
+      <div class="date">{{item.date}}</div>
+      <div class="pendingName">{{item.pendingName}}</div>
+      <el-checkbox v-model="item.status" class="checkbox" @change="changeChildren($event,index)" />
+    </div>
+
+  </div>
 </div>
 </template>
 
@@ -21,16 +38,20 @@ export default {
   },
   data() {
     return {
-      clickFlag:false,
-      checked:false,
+      clickFlag: false,
+      checked: false,
+      xxx: false
     }
   },
   methods: {
     expandList() {
       this.clickFlag = !this.clickFlag;
     },
-    change() {
-      console.log('change');
+    change(value) {
+      console.log('change', value);
+    },
+    changeChildren(value, index) {
+      console.log('children', index, ' is ', value);
     }
   },
 };
@@ -38,54 +59,71 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-  display: flex;
-  align-items: center;
   background: #FFE9E8;
   margin-top: 4px;
   border-radius: 4px;
-  height: 48px;
   font-family: PingFangSC-Regular;
   font-size: 16px;
-  color: rgba(0,0,0,0.65);
+  color: rgba(0, 0, 0, 0.65);
   letter-spacing: 0;
   font-weight: 400;
 }
+
+.father {
+  display: flex;
+  align-items: center;
+}
+
+.children {
+  display: flex;
+  align-items: center;
+}
+
 .date {
   padding: 0 16px;
   height: 48px;
   line-height: 48px;
   border-right: 1px solid #F3F9FF;
 }
+
 .el-icon-arrow-right {
   cursor: pointer;
   font-size: 15px;
   margin-right: 5px;
 }
+
 .missionName {
-  width: 400px;
+  width: 600px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
   padding-left: 16px;
   height: 48px;
   line-height: 48px;
-}
-.pendingName {
-  padding: 0 16px;
-  height: 48px;
-  line-height: 48px;
   border-right: 1px solid #F3F9FF;
 }
-.checkbox{
+
+.pendingName {
+  display: flex;
+  align-items: center;
+  padding: 0 16px;
+  width: 130px;
+  height: 48px;
+  border-right: 1px solid #F3F9FF;
+}
+
+.checkbox {
   padding: 0 16px;
   height: 48px;
   line-height: 48px;
 }
+
 .rightArrow {
   transition: 0.2s;
   transform-origin: center;
   transform: rotate(90deg);
 }
+
 .downArrow {
   transition: 0.2s;
   transform-origin: center;
--- src/renderer/views/pending/index.vue
+++ src/renderer/views/pending/index.vue
@@ -6,20 +6,62 @@
         <span slot="label" class="title">我的待办</span>
         <div class="titleCenter">
           <el-input placeholder="请输入内容" prefix-icon="el-icon-search" class="searchInput" v-model="searchInput" />
-          <i class="el-icon-circle-plus-outline" />
+          <i class="el-icon-circle-plus-outline" @click="dialogVisible = true "/>
         </div>
         <PendingList v-for="(item,key) of tableData" :key="key" :detail="item" />
-      </el-tab-pane>
+      </el-tab-pane> 
       <el-tab-pane name="second">
         <span slot="label" class="title">授权的待办</span>
       </el-tab-pane>
     </el-tabs>
+    <el-dialog
+      title="新增待办 "
+      :visible.sync="dialogVisible"
+      width="30%">
+      <div class="dialogContent">
+        <div class="dialogLine" style="margin-bottom: 8px" >
+          <div class="dialogText">待办时间</div>
+          <el-time-picker
+            v-model="pendingTime"
+            style="margin: 0 8px;width:224px;" 
+            :picker-options="{
+              selectableRange: '7:30:00 - 23:59:59'
+            }"
+            placeholder="选择时间"/>
+        </div>
+        <div class="dialogLine" style="margin-bottom: 8px">
+          <div class="dialogText">待办事项</div>
+          <el-input v-model="pengingSth" style="margin: 0 8px;width:224px;" placeholder="请输入" @change='test'/>
+        </div>
+        <div class="dialogLine" style="margin-bottom: 8px">
+          <div class="dialogText">需求编号</div>
+          <el-select v-model="mission" style="margin: 0 8px;width:224px" placeholder="请选择（可为空）">
+            <el-option v-for="item in missionList" :key="item.missionNo" :label="item.missionName" :value="item.missionNo" ></el-option>
+          </el-select>
+        </div>
+        <div class="dialogLine" style="margin-bottom: 8px">
+          <div class="dialogText">待办人</div>
+          <el-select v-model="investor" style="margin: 0 8px;width:224px" placeholder="请选择">
+            <el-option v-for="item in investorList" :key="item.investorNo" :label="item.investorName" :value="item.investorNo" ></el-option>
+          </el-select>
+        </div>
+        <div class="dialogLine">
+          <div class="dialogText">提  醒</div>
+          <el-switch v-model="remindSwitch" style="margin:0 32px 0 8px" />
+          <div class="smallText">开始前<el-input-number v-model="inputMin" controls-position="right" style="width:96px;margin:0 8px;" :min="1" :max="120" />分钟提醒我</div>
+        </div>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </div>
 </template>
 
 <script>
-import PendingList from './components/pendingList.vue';
+import PendingList from './components/pendingList2.vue';
 import {
   mapGetters
 } from "vuex";
@@ -30,29 +72,110 @@ export default {
     return {
       activeName: 'first',
       searchInput: '',
+      dialogVisible:false,
+      pendingTime:'',
+      pengingSth:'',
+      mission:'',
+      missionList:[{missionNo:'UT-WLJR-2022-0575',missionName:'测试4'},{missionNo:'UT-WLJR-2022-0525',missionName:'测试3'},{missionNo:'UT-WLJR-2022-0505',missionName:'测试2'},{missionNo:'KEJI-2022-0575',missionName:'测试1'},],
+      investor: '00000000',
+      investorList:[{investorNo:'00000000',investorName:'本人'},{investorNo:'12066390',investorName:'李亚威'},],
+      remindSwitch:false,
+      inputMin: 15,
       tableData: [{
         id: 1,
-        date: '2016-05-04',
         missionName: 'UT-WLJR-2022-0575-手机银行财富待办微信通知及场景化开通提醒业务需求(结构性)-mspmk-cli-structdeposits',
-        pendingName: '提交内测',
+        status:false,
         children: [{
           pendingName: '提交内测',
-          date: '2016-05-04',
+          date: '2016-05-05',
+          status:false
         }, {
-          pendingName: '提交内测',
-          date: '2016-05-04',
+          pendingName: '上传资料',
+          date: '2016-05-06',
+          status:false
+        }, {
+          pendingName: '提交业测',
+          date: '2016-05-07',
+          status:false
+        }, {
+          pendingName: 'rel代码审核',
+          date: '2016-05-07',
+          status:false
+        }, {
+          pendingName: '投产',
+          date: '2016-05-10',
+          status:false
         }]
       },{
-        id: 1,
-        date: '2016-05-04',
+        id: 2,
         missionName: 'UT-WLJR-2022-0575-手机银行财富待办微信通知及场景化开通提醒业务需求(结构性)-mspmk-cli-structdeposits',
-        pendingName: '提交内测',
+        status:false,
+        children: [{
+          pendingName: '提交内测',
+          date: '2016-05-05',
+          status:false
+        }, {
+          pendingName: '上传资料',
+          date: '2016-05-06',
+          status:false
+        }, {
+          pendingName: '提交业测',
+          date: '2016-05-07',
+          status:false
+        }, {
+          pendingName: 'rel代码审核',
+          date: '2016-05-07',
+          status:false
+        }, {
+          pendingName: '投产',
+          date: '2016-05-10',
+          status:false
+        }]
+      },{
+        id: 3,
+        missionName: 'UT-WLJR-2022-0575-手机银行财富待办微信通知及场景化开通提醒业务需求(结构性)-mspmk-cli-structdeposits',
+        status:false,
+        children: [{
+          pendingName: '提交内测',
+          date: '2022-09-14',
+          status:false
+        }, {
+          pendingName: '上传资料',
+          date: '2022-09-14',
+          status:false
+        }, {
+          pendingName: '提交业测',
+          date: '2022-09-14',
+          status:false
+        }, {
+          pendingName: 'rel代码审核',
+          date: '2022-09-14',
+          status:false
+        }, {
+          pendingName: '投产',
+          date: '2022-09-14',
+          status:false
+        }]
+      },
+      {
+        id: 2,
+        missionName: '会议室2801开会',
+        status:false,
+        children: [{
+          pendingName: '其他',
+          date: '2028-03-05 10:33:00',
+          status:false
+        }]
       }]
     }
   },
   computed: {
     ...mapGetters(["name", "roles"]),
   },
+  methods: {
+    test(){
+    }
+  }
 };
 </script>
 
@@ -91,12 +214,12 @@ export default {
   padding: 0 10px;
 }
 
-::v-deep .el-input__inner {
+::v-deep .titleCenter .el-input--prefix .el-input__inner {
   padding-left: 35px;
   border-radius: 20px;
 }
 
-::v-deep .el-input__icon {
+::v-deep .titleCenter .el-input__icon {
   padding-left: 20px;
 }
 
@@ -113,5 +236,25 @@ export default {
   font-weight: 100;
   font-size: 35px;
 }
-
+.dialogLine {
+  display: flex;
+  align-items: center;
+  line-height: 32px;
+}
+.dialogText {
+  width: 56px;
+  text-align: right;
+  font-family: PingFangSC-Regular;
+  font-size: 14px;
+  color: rgba(0,0,0,0.65);
+  letter-spacing: 0;
+  font-weight: 400;
+}
+.smallText{
+  font-family: PingFangSC-Regular;
+  font-size: 12px;
+  color: rgba(0,0,0,0.45);
+  letter-spacing: 0;
+  font-weight: 400;
+}
 </style>
